f7e2fb64591183ce6fa9087de525296ae35f304c
Miquel Sabaté <mikisabate@gmail.com>
2011-09-19 22:00:53 +0200

    Initial commit

---
+++ rem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rem::Application.config.secret_token = '94fc7d27842e63d5396adf697fc933db58f1565e4db193af0adb490e3b13f5c89406513074aa1f0b22192dfe50ea3befd04b2fef3fdfb763c92f113b3dc390fa'
