db9699962ebdc0b7621af86c0d0e52eb54acc449
Miquel Sabaté <mikisabate@gmail.com>
2011-10-03 18:43:47 +0200

    Documenting the password reset controller

--- rem/app/controllers/password_resets_controller.rb
+++ rem/app/controllers/password_resets_controller.rb
@@ -21,20 +21,38 @@
 ##
 # == PasswordResetsController Class Definition
 #
-# NOTE: Under construction
+# This is the password resets controller. It handles the logic behind
+# resetting passwords. It sends an email when the user wants to reset his
+# password. Inside this email, there are some instructions about how
+# to properly reset the password and an url pointing to the edit method.
+# In the edit page the user will be able to change his password.
 class PasswordResetsController < ApplicationController
+  ##
+  # The _new_ method. It does nothing.
   def new; end
 
+  ##
+  # The _create_ method. It's called when the user submitted the password
+  # reset form. If everything is fine, it will send an email to the given
+  # account with instructions inside on how to change his password and a link
+  # pointing to the edit method.
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
     redirect_to root_url, :notice => _('Email sent with password reset instructions.')
   end
 
+  ##
+  # The _edit_ method. It handles the logic behind the form for changing
+  # the password.
   def edit
     @user = User.find_by_password_reset_token!(params[:id])
   end
 
+  ##
+  # The _update_ method. It's called when the user has submitted the edit
+  # form. This method does the actual password resetting and finally
+  # it redirects to the root url.
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
