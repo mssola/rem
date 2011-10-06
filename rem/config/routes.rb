4b29e9df92a4eb0b56b0f107863618be33dd78d4
Miquel Sabaté <mikisabate@gmail.com>
2011-10-06 15:47:39 +0200

    Fixing some routes

--- rem/config/routes.rb
+++ rem/config/routes.rb
@@ -9,11 +9,11 @@ Rem::Application.routes.draw do
   root to: 'home#index'
 
   # Footer
-  get 'about' => "home#about"
-  get 'overview' => "home#overview"
-  get 'contact' => "home#contact"
-  get 'api' => "home#api"
-  get 'help' => "home#help"
+  get '/about' => "home#about"
+  get '/overview' => "home#overview"
+  get '/contact' => "home#contact"
+  get '/api' => "home#api"
+  get '/help' => "home#help"
 
   # Rest API
   get "/users/:name" => "users#show"
