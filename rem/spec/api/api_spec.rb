bcf224d4562dd8e2ef46d0cebf2ee71ab54ce897
Miquel Sabaté <mikisabate@gmail.com>
2011-10-08 19:20:19 +0200

    Improved the API test

--- rem/spec/api/api_spec.rb
+++ rem/spec/api/api_spec.rb
@@ -1,26 +1,37 @@
 
+$: << File.dirname(__FILE__)
+
 require 'spec_helper'
-require File.dirname(__FILE__) + '/request'
+require 'request'
 
 
 describe 'Rest API' do
-  Request.init('http://localhost:3000')
+  Reqs.init('http://localhost:3000')
 
   describe 'User' do
-    # TODO: this user has to be on our database, it can be improved
-    let(:user) { Factory(:user, :name => 'mssola') }
+    # This user has to be on our database
+    # TODO can be improved
+    let(:user) { Factory(:user, :name => 'mssola',
+                         :email => 'mikisabate@gmail.com') }
+
+    # This user is on our database
     let(:bad)  { Factory(:user, :name => 'i_am_not_in_the_db') }
 
     it 'sends a valid JSON object when a user has been requested' do
-      response = Request.do_get("/users/#{user.name}")
+      response = Reqs.do_get("/users/#{user.name}")
       response[:name].should eql(user.attributes[:name])
       response[:email].should eql(user.attributes[:email])
     end
 
     it 'raises an exception when trying to access an invalid user' do
-      expect { Request.do_get("/users/#{user.bad}") }.to raise_error(NoMethodError)
+      expect { Reqs.do_get("/users/#{user.bad}") }.to raise_error(NoMethodError)
+      expect { Reqs.do_get("/users/#{user.bad}.xml") }.to raise_error(NoMethodError)
     end
 
-    pending 'same as above but with XML'
+    it 'sends a valid XML object when a user has been requested' do
+      response = Reqs.do_get("/users/#{user.name}.xml")
+      Reqs.xml_compare(response, 'name', user.attributes['name'])
+      Reqs.xml_compare(response, 'email', user.attributes['email'])
+    end
   end
 end
--- rem/spec/api/request.rb
+++ rem/spec/api/request.rb
@@ -1,11 +1,51 @@
 
-module Request
+require 'libxml'
+
+
+##
+# == Reqs Module Definition
+#
+# Helper module for Requests/Responses. It does get requests according
+# to the Rest API specification and it returns a proper response object.
+module Reqs
+  include LibXML
+
+  ##
+  # Initialize the url where all the requests will point.
+  #
+  # @param *String* url A valid http url.
   def self.init(url)
     @url = URI.parse(url)
   end
 
+  ##
+  # Do a GET request according to the Rest API specification.
+  #
+  # @param *String* path Relative path to the base url specified during
+  # the initialization of this module.
+  #
+  # @return A JSON or an XML object.
   def self.do_get(path)
     res = Net::HTTP.start(@url.host, @url.port) { |http| http.get(path) }
-    JSON.parse res.body
+    if path.end_with?('.xml')
+      res = XML::Document.string(res.body)
+    else
+      res = JSON.parse res.body
+    end
+    res
+  end
+
+  ##
+  # Compare an XML element value with the expected one. Raises an
+  # ArgumentError if the element value hasn't the expected value.
+  #
+  # @param *XML::Document* doc The XML document.
+  #
+  # @param *String* element The element we are looking for.
+  #
+  # @param *String* should_eql The expected value for the given element.
+  def self.xml_compare(doc, element, should_eql)
+    e = doc.find("//#{element}").first.inner_xml
+    raise ArgumentError unless e == should_eql
   end
 end
