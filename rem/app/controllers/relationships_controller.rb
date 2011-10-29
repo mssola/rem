ca7932a2fb0f2b794314a9571fa323f96e45cb12
Miquel Sabaté <mikisabate@gmail.com>
2011-10-29 13:33:32 +0200

    No AJAX for now since it's not really working

--- rem/app/controllers/relationships_controller.rb
+++ rem/app/controllers/relationships_controller.rb
@@ -21,26 +21,16 @@
 ##
 # TODO
 class RelationshipsController < ApplicationController
-#   respond_to :html, :js
-#   before_filter :authenticate
 
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-#     respond_with( "/#{@user.name}", :layout => !request.xhr? )
-    respond_to do |format|
-      format.html { redirect_to "/#{@user.name}" }
-      format.js
-    end
+    redirect_to "/#{@user.name}"
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-#     respond_with( "/#{@user.name}", :layout => !request.xhr? )
-    respond_to do |format|
-      format.html { redirect_to "/#{@user.name}" }
-      format.js
-    end
+    redirect_to "/#{@user.name}"
   end
 end 
