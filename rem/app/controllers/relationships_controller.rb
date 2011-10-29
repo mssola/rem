9e01f8e051da985cb6af94c40399eaf893e0b94b
Miquel Sabaté <mikisabate@gmail.com>
2011-10-29 13:37:58 +0200

    Documenting the RelationshipsController

--- rem/app/controllers/relationships_controller.rb
+++ rem/app/controllers/relationships_controller.rb
@@ -19,15 +19,23 @@
 
 
 ##
-# TODO
+# == RelationshipsController Class Definition
+#
+# This is the controller for the relationships. In particular, it
+# handles when a user wants to follow/unfollow another user.
 class RelationshipsController < ApplicationController
-
+  ##
+  # The _create_ method. The user wants to follow another user. After
+  # that, we redirect the user to the same page.
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
     redirect_to "/#{@user.name}"
   end
 
+  ##
+  # The _destroy_ method. The user wants to unfollow another user. We
+  # redirect the user to the same page.
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
