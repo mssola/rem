35d1b03806f91b6eacfbbe12b2395532ee797775
Miquel Sabaté <mikisabate@gmail.com>
2011-11-18 17:35:25 +0100

    Fixed some issues on compiling css in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rem::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
+  config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
