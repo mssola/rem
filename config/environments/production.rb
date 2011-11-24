b78aa959ce3ed8a9c1a13cc2ba5974e72e8eda7b
Miquel Sabaté <mikisabate@gmail.com>
2011-11-25 00:41:42 +0100

    Added the gmail_smtp rails plugin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,6 @@ Rem::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # Remember setting the necessery variables into heroku
   config.action_mailer.default_url_options = { :host => "mssola.heroku.com" }
 end
